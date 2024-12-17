3f3c9278599ec40e572b3b2ba6a1d99341d881eb
Ruby Edwards <ruby.vte@gmail.com>
2024-12-17 11:50:13 +0000

    create loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <iframe
+      src="https://lottie.host/embed/e8f309a0-3a05-4f8f-891f-b4933a967aca/QRuomyYuix.lottie"
+      frameborder="0"
+    ></iframe>
+  );
+};
+
+export default Loading;
