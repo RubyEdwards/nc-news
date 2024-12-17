ba33c04e6aeba05c5cdab978a0456ea9ac15765c
Ruby Edwards <ruby.vte@gmail.com>
2024-12-17 16:48:57 +0000

    refactor date formatting into function

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -5,15 +5,17 @@ const ArticleCard = ({ article }) => {
   const { author, title, article_id, topic, created_at, votes, comment_count } =
     article;
 
-  const date = new Date(created_at);
-  let min = date.getMinutes();
-  let hr = date.getHours();
-  const dd = date.getDate();
-  const yy = date.getFullYear();
-  const mm = date.getMonth() + 1;
-  min = min < 10 ? `0${min}` : min;
-  hr = hr < 10 ? `0${hr}` : hr;
-  const dateStr = `${hr}:${min} ${dd}/${mm}/${yy}`;
+  const formatDate = () => {
+    const date = new Date(created_at);
+    let min = date.getMinutes();
+    let hr = date.getHours();
+    const dd = date.getDate();
+    const yy = date.getFullYear();
+    const mm = date.getMonth() + 1;
+    min = min < 10 ? `0${min}` : min;
+    hr = hr < 10 ? `0${hr}` : hr;
+    return `${hr}:${min} ${dd}/${mm}/${yy}`;
+  };
 
   const handleClick = () => {
     navigate(`/articles/${article_id}`);
@@ -25,13 +27,15 @@ const ArticleCard = ({ article }) => {
       <p id="author">by {author}</p>
       <ul id="article-info">
         <li>Topic: {topic}</li>
-        <li>Posted at {dateStr}</li>
+        <li>Posted at {formatDate()}</li>
         <li>{votes} votes</li>
         <li>{comment_count} comments</li>
       </ul>
-      <button type="button" onClick={handleClick}>
-        READ THIS ARTICLE
-      </button>
+      <div id="article-buttons">
+        <button type="button" onClick={handleClick}>
+          READ THIS ARTICLE
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,22 +1,24 @@
 const CommentCard = ({ comment }) => {
   const { comment_id, body, author, votes, created_at } = comment;
 
-  const date = new Date(created_at);
-  let min = date.getMinutes();
-  let hr = date.getHours();
-  const dd = date.getDate();
-  const yy = date.getFullYear();
-  const mm = date.getMonth() + 1;
-  min = min < 10 ? `0${min}` : min;
-  hr = hr < 10 ? `0${hr}` : hr;
-  const dateStr = `${hr}:${min} ${dd}/${mm}/${yy}`;
+  const formatDate = () => {
+    const date = new Date(created_at);
+    let min = date.getMinutes();
+    let hr = date.getHours();
+    const dd = date.getDate();
+    const yy = date.getFullYear();
+    const mm = date.getMonth() + 1;
+    min = min < 10 ? `0${min}` : min;
+    hr = hr < 10 ? `0${hr}` : hr;
+    return `${hr}:${min} ${dd}/${mm}/${yy}`;
+  };
 
   return (
     <div id="comment-card">
       <p>
         {body} - {author}
       </p>
-      <p>Posted at {dateStr}</p>
+      <p>Posted at {formatDate()}</p>
       <p>{votes} votes</p>
       <p>Comment no. {comment_id}</p>
     </div>
