c9ae719a5de2ba34270cc487b9c0847651bd3cc5
Ruby Edwards <ruby.vte@gmail.com>
2024-12-18 12:14:38 +0000

    refactor for readabililty

--- src/components/SingleArticleView.jsx
+++ src/components/SingleArticleView.jsx
@@ -1,16 +1,15 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getArticle } from "../app";
 import Loading from "./Loading";
 import Article from "./Article";
 import CommentList from "./CommentList";
+import { Link } from "react-router-dom";
 
 const SingleArticleView = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     setIsLoading(true);
@@ -20,37 +19,29 @@ const SingleArticleView = () => {
     });
   }, []);
 
-  const handleClick = () => {
-    navigate("/articles");
-  };
-
   if (isLoading) {
     return (
       <>
         <Loading />
-        <button type="button" onClick={handleClick}>
-          BACK
-        </button>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <p>You are viewing article {article_id}!</p>
-        <section id="single-article-view">
-          <div id="article-column">
-            <Article article={article} />
-            <button type="button" onClick={handleClick}>
-              BACK
-            </button>
-          </div>
-          <div id="comments-column">
-            <CommentList article_id={article_id} />
-          </div>
-        </section>
+        <Link to="/articles">BACK</Link>
       </>
     );
   }
+
+  return (
+    <>
+      <p>You are viewing article {article_id}!</p>
+      <section id="single-article-view">
+        <div id="article-column">
+          <Article article={article} />
+          <Link to="/articles">BACK</Link>
+        </div>
+        <div id="comments-column">
+          <CommentList article_id={article_id} />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SingleArticleView;
