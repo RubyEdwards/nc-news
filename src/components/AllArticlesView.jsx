95508fbe5439b73d5748e50eb893338c6cc5dd19
Ruby Edwards <ruby.vte@gmail.com>
2024-12-19 16:36:45 +0000

    display articles by topic

--- src/components/AllArticlesView.jsx
+++ src/components/AllArticlesView.jsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 import { getArticles } from "../app";
 import Loading from "./Loading";
 import ArticleList from "./ArticleList";
+import { useSearchParams } from "react-router-dom";
 
 const AllArticlesView = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const topicQuery = searchParams.get("topic");
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
-    getArticles().then((fetchedArticles) => {
+    getArticles(topicQuery).then((fetchedArticles) => {
       setArticles(fetchedArticles);
       setIsLoading(false);
     });
-  }, []);
+  }, [topicQuery]);
 
   if (isLoading) {
     return <Loading />;
@@ -21,7 +24,7 @@ const AllArticlesView = () => {
 
   return (
     <>
-      <h2>Articles:</h2>
+      <h2>Articles{topicQuery ? ` for ${topicQuery}` : null}:</h2>
       <ArticleList articles={articles} />
     </>
   );
