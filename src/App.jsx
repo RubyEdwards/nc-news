a7483edcbecaa56f6b586d72e8a666f0482ec39e
Ruby Edwards <ruby.vte@gmail.com>
2025-01-04 14:40:22 +0000

    use React Bootstrap components for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import AllArticlesView from "./components/AllArticlesView";
 import SingleArticleView from "./components/SingleArticleView";
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
       <Header />
-      <Nav />
       <Routes>
         <Route path="/" element={<AllArticlesView />} />
         <Route path="/articles" element={<AllArticlesView />} />
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,11 +1,21 @@
 import ArticleCard from "./ArticleCard";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const ArticleList = ({ articles }) => {
   return (
     <section id="article-list">
-      {articles.map((article) => {
-        return <ArticleCard key={article.article_id} article={article} />;
-      })}
+      <Row xs={1} md={2} xl={3} xxl={4} className="g-3">
+        {articles.map((article, idx) => {
+          return (
+            <>
+              <Col key={idx}>
+                <ArticleCard key={article.article_id} article={article} />
+              </Col>
+            </>
+          );
+        })}
+      </Row>
     </section>
   );
 };
